2691a4758f786339c77bf03e43509ac1e25c0be5
Spencer Rauch <rauchs@gmail.com>
2023-05-22 06:55:38 -0700

    flip flop order

--- algos/W4D1.js
+++ algos/W4D1.js
@@ -1,38 +1,3 @@
-/* 
-Recursive Sigma
-Input: integer
-Output: sum of integers from 1 to Input integer
-*/
-
-const num1 = 5;
-const expected1 = 15;
-// Explanation: (1+2+3+4+5)
-
-const num2 = 2.5;
-const expected2 = 3;
-// Explanation: (1+2)
-
-const num3 = -1;
-const expected3 = 0;
-
-/**
- * Recursively sum the given int and every previous positive int.
- * - Time: O(?).
- * - Space: O(?).
- * @param {number} num
- * @returns {number}
- */
-function recursiveSigma(num) {
-  //Base case?
-  //Any logic?
-  //Recursion?
-
-}
-
-console.log(recursiveSigma(num1)); // 15
-console.log(recursiveSigma(num2)); // 3
-console.log(recursiveSigma(num3)); // 0
-
 /*****************************************************************************/
 
 /* 
@@ -73,3 +38,40 @@ console.log(sumArr(numsA)) // 6
 console.log(sumArr(numsB)) // 1
 console.log(sumArr(numsC)) // 0
 /*****************************************************************************/
+
+
+/* 
+Recursive Sigma
+Input: integer
+Output: sum of integers from 1 to Input integer
+*/
+
+const num1 = 5;
+const expected1 = 15;
+// Explanation: (1+2+3+4+5)
+
+const num2 = 2.5;
+const expected2 = 3;
+// Explanation: (1+2)
+
+const num3 = -1;
+const expected3 = 0;
+
+/**
+ * Recursively sum the given int and every previous positive int.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {number} num
+ * @returns {number}
+ */
+function recursiveSigma(num) {
+  //Base case?
+  //Any logic?
+  //Recursion?
+
+}
+
+console.log(recursiveSigma(num1)); // 15
+console.log(recursiveSigma(num2)); // 3
+console.log(recursiveSigma(num3)); // 0
+
