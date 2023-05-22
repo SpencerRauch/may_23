d986c0cfc4a6a27b2130cff91ca39174cb516d36
Spencer Rauch <rauchs@gmail.com>
2023-05-22 10:30:37 -0700

    templates demo

--- flask/templates/flask_app/controllers/routes.py
+++ flask/templates/flask_app/controllers/routes.py
@@ -16,7 +16,7 @@ cat_toys = [
 @app.route('/')
 @app.route('/home')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
